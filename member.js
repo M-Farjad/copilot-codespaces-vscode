54beacbe7c804adf9c867e7619e138819bfff184
Farjad Waseem <99951985+M-Farjad@users.noreply.github.com>
2023-07-24 13:10:56 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+// function skillsMember()
+function skillsMember(){
+    return: {
+        restrict: 'E',
+        templateURL: 'modules/skills/views/member.html',
+        controller: 'SkillsMemberController',
+        controllerAs: 'vm',
+        bindToController: true,
+        scope: {
+            member: '='
+        }
+    };
+}
